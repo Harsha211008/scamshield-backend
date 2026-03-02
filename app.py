b86909ce062abdc852941539c78751e0182204d5
Harsha211008 <harshayandamuri2008@gmail.com>
2026-03-02 19:10:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,5 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import pickle
-
-app = Flask(__name__)
-CORS(app)
-
-# Load trained model
-model = pickle.load(open("scam_model.pkl", "rb"))
-vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
-@app.route('/', methods=['GET'])
-def home():
-    return jsonify({"status": "ScamAlert AI running"})
-@app.route('/predict', methods=['POST'])
+from flask import Flask, request, jsonify@app.route('/predict', methods=['POST'])
 def predict():
-
     try:
         data = request.get_json()
 
@@ -28,8 +14,9 @@ def predict():
         # Limit extremely long text
         message = message[:500]
 
-       vectorized = vectorizer.transform([message])
-prediction = model.predict(vectorized)[0]
+        # Use vectorizer properly
+        vectorized = vectorizer.transform([message])
+        prediction = model.predict(vectorized)[0]
 
         text = message.lower()
 
@@ -49,5 +36,3 @@ prediction = model.predict(vectorized)[0]
 
     except:
         return jsonify({"category": 0})
-
-
